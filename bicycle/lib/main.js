dd088de7dc1918208c630f1a6bb6b218a42c6891
rouesh <master_rpg@mail.ru>
2021-05-02 00:31:33 +0700

    Added responsive for header-menu

---
+++ bicycle/lib/main.js
@@ -0,0 +1,11 @@
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.menu__body');
+    const menuCloseItem = document.querySelector('.burger__close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('menu__body_active');
+    })
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('menu__body_active');
+    })
+} ());
