8c362ce394676f35aeb08228c72e441ed4c37943
rouesh <master_rpg@mail.ru>
2021-05-06 21:02:11 +0700

    Created section shop

--- bicycle/js/main.js
+++ bicycle/js/main.js
@@ -17,3 +17,14 @@ $(document).ready(function(){
         adaptiveHeight:true
     });
 });
+
+function ibg(){
+
+    $.each($('.ibg'), function(index, val) {
+    if($(this).find('img').length>0){
+    $(this).css('background-image','url("'+$(this).find('img').attr('src')+'")');
+    }
+    });
+    }
+    
+    ibg();
