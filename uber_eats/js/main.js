eaaa4b5d13f686e65e512513f28f7d98724abfd8
rouesh <master_rpg@mail.ru>
2021-05-14 11:16:28 +0700

    Added rest__slider

--- uber_eats/js/main.js
+++ uber_eats/js/main.js
@@ -1,8 +1,20 @@
 $(document).ready(function(){
-    $('.slider__body').slick({
+    $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
         arrows:false,
         dots:false,
         adaptiveHeight:true,
-        draggable:false
-    });
+        draggable:false,
+        asNavFor: '.slider-nav'
+      });
+      $('.slider-nav').slick({
+        slidesToShow: 6,
+        slidesToScroll: 6,
+        asNavFor: '.slider-for',
+        dots: false,
+        arrows:false,
+        centerMode: true,
+        focusOnSelect: true
+      });
 });
